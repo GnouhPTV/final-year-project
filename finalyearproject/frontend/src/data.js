feaf3ac4b04bcf82363ad9ce82fad82f8f9e9bdc
GnouhPTV <nhocphuongtk3@gmail.com>
2023-03-28 04:05:36 -0700

    create Featured Products List

---
+++ finalyearproject/frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+  products: [
+    {
+      name: 'Volcanic Clay',
+      slug: 'volcanic-clay',
+      category: 'Waxes',
+      image: '/images/p1.jpg',
+      price: 10,
+      countInStock: 10,
+      brand: 'Apestomen',
+      rating: '4.5',
+      numReviews: '10',
+      description:
+        'Sáp vuốt tóc Volcanic Clay với sự thay đổi lớn về màu sắc của vỏ hộp cho riêng thị trường Việt Nam đã xuất hiện. Phiên bản này vẫn giữ nguyên thành phần, mùi hương và độ giữ nếp giống như bản cũ, nhưng vỏ hộp được thay đổi bằng màu đen tuyền ngầu hơn và nam tính hơn.',
+    },
+    {
+      name: 'Reuzel Pink Pomade',
+      slug: 'reuzel-pink-pomade',
+      category: 'Pomades',
+      image: '/images/p2.jpg',
+      price: 25,
+      countInStock: 11,
+      brand: 'Reuzel',
+      rating: '4.5',
+      numReviews: '14',
+      description:
+        'Dòng pomade tuyệt vời và linh hoạt nhất của Reuzel, có bổ sung thêm sáp ong với khả năng giữ nếp tốt và mang mùi hương thơm phức của kẹo cao su.',
+    },
+    {
+      name: 'Reuzel Blue Pomade',
+      slug: 'reuzel-blue-pomade',
+      category: 'Pomades',
+      image: '/images/p3.jpg',
+      price: 24,
+      countInStock: 9,
+      brand: 'Reuzel',
+      rating: '5',
+      numReviews: '13',
+      description:
+        'Cảm nhận về sản phẩm Reuzel Blue Pomade có thể nói là độ giữ nếp rất tốt, tóc ẩm và mùi hương có thể được gọi một loại nước hoa.',
+    },
+    {
+      name: 'NUNCHI Matte Clay',
+      slug: 'nunchi-matte-clay',
+      category: 'Waxes',
+      image: '/images/p4.jpg',
+      price: 19,
+      countInStock: 8,
+      brand: 'Nunchi',
+      rating: '4.7',
+      numReviews: '20',
+      description:
+        'NUNCHI (Hàn Quốc - danh từ) - nghệ thuật và khả năng TINH TẾ để có thể nắm bắt, đánh giá CHÍNH XÁC trong bất kỳ tình huống nào rồi từ đó phản ứng một cách thích đáng.  ',
+    },
+  ],
+};
+
+export default data;
